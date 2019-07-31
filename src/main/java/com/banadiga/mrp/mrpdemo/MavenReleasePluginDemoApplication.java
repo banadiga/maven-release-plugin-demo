99d078d37fa16faf251e6aa3e505bb14f88ac528
Ihor Banadiga <banadiga@gmail.com>
2019-07-31 23:01:31 +0300

    init project

---
+++ src/main/java/com/banadiga/mrp/mrpdemo/MavenReleasePluginDemoApplication.java
@@ -0,0 +1,13 @@
+package com.banadiga.mrp.mrpdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenReleasePluginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenReleasePluginDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banadiga/mrp/mrpdemo/MavenReleasePluginDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.banadiga.mrp.mrpdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MavenReleasePluginDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
